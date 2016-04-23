de4898f8b72654f9250be8cbc4a9287ac2caa62a
Roman <admiralexo@gmail.com>
2016-04-23 21:12:57 +0300

    Post & Markdown edit

---
+++ app/Jobs/PostFormFields.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Jobs;
+
+use App\Jobs\Job;
+use Illuminate\Queue\SerializesModels;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Contracts\Queue\ShouldQueue;
+
+class PostFormFields extends Job implements ShouldQueue
+{
+    use InteractsWithQueue, SerializesModels;
+
+    /**
+     * The id (if any) of the Post row
+     *
+     * @var integer
+     */
+    protected $id;
+
+    /**
+     * List of fields and default value for each field
+     *
+     * @var array
+     */
+    protected $fieldList = [
+        'title' => '',
+        'subtitle' => '',
+        'page_image' => '',
+        'content' => '',
+        'meta_description' => '',
+        'is_draft' => "0",
+        'publish_date' => '',
+        'publish_time' => '',
+        'layout' => 'blog.layouts.post',
+        'tags' => [],
+    ];
+
+    /**
+     * Create a new job instance.
+     *
+     * PostFormFields constructor.
+     * @param null $id
+     */
+    public function __construct($id = null)
+    {
+        $this->id = $id;
+    }
+
+    /**
+     * Execute the job.
+     *
+     * @return array
+     */
+    public function handle()
+    {
+        $fields = $this->fieldList;
+
+        if ($this->id) {
+            $fields = $this->fieldsFromModel($this->id, $fields);
+        } else {
+
+        }
+    }
+}
